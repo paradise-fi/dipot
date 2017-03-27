e482c8aea873aba4eb28baa0191c2316c5a5f131
xlauko <xlauko@mail.muni.cz>
2017-03-27 14:40:01 +0200

    Setup dipot driver.

---
+++ dipot.cpp
@@ -0,0 +1,6 @@
+#include "driver.h"
+
+int main( int argc, char const *argv[] ) {
+    dipot::Driver driver( argc, argv );
+    return driver.run();
+}
---
+++ driver.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <brick-cmd>
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+#include <spot/tl/parse.hh>
+#include <spot/tl/print.hh>
+#include <spot/misc/version.hh>
+#include <spot/twaalgos/translate.hh>
+#include <spot/twaalgos/neverclaim.hh>
+#include <spot/twaalgos/cformat.hh>
+
+#include "format/cformat.h"
+
+namespace dipot {
+
+struct Formula {
+    std::string formula;
+};
+
+struct Driver {
+    std::vector< std::string > args;
+
+    Driver( int argc, const char *argv[] ) :
+        args( brick::cmd::from_argv( argc, argv ) )
+    { }
+
+    template< typename Parser >
+    auto parse( Parser p ) {
+        return p.parse( args.begin(), args.end() );
+    }
+
+    auto commands() {
+        auto v = brick::cmd::make_validator();
+        auto fopts = brick::cmd::make_option_set< Formula >( v )
+            .option( "[-f {string}]", &Formula::formula, "ltl formula" );
+        return brick::cmd::make_parser( v ).command< Formula >( fopts );
+    }
+
+    int run() {
+        try {
+            auto cmds = commands();
+            auto cmd = parse( cmds );
+
+            if ( cmd.empty() ) {
+                Help().run( cmds );
+                return 0;
+            }
+
+     //       cmd.match( [&]( Help & help ) {
+       //                    help.run( cmds );
+         //              },
+           //            [&]( Command & c ) {
+             //              c.setup();
+               //            c.run();
+                 //      } );
+            return 0;
+        } catch( brick::except::Error &e ) {
+            std::cerr << "ERROR: " << e.what() << std::endl;
+            return e._exit;
+        }
+    }
+
+    void process() {}
+private :
+
+
+};
+
+} // namespace dipot
+
+/*int main( int argc, char **argv ) {
+    const std::string formula = "isZero U isOne";
+    spot::parsed_formula pf = spot::parse_infix_psl(formula);
+    if (pf.format_errors(std::cerr))
+        return 1;
+    spot::translator trans;
+    trans.set_type(spot::postprocessor::BA);
+    spot::twa_graph_ptr aut = trans.run(pf.f);
+    dipot::print_c_format(std::cout, aut);
+    return 0;
+}*/
